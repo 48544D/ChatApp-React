d3e9c4bfa82b936886f4cd70dec16928adae5894
HTM <haythamaaraba7@gmail.com>
2023-08-06 17:14:36 +0100

    bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,19 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Chat />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <Container className="text-secondary">
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
   );
 }
 
